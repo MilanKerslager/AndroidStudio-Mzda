dadd2a1cef2d93ed1f34f34dc8ed671be43ce3b1
Milan Kerslager <milan.kerslager@pslib.cz>
2017-03-16 22:49:41 +0100

    comments, code formatting

--- app/src/main/java/cz/kerslager/android/mzda/MainActivity.java
+++ app/src/main/java/cz/kerslager/android/mzda/MainActivity.java
@@ -22,28 +22,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonKlik(View view) {
         // https://www.vypocet.cz/popis-vypoctu-ciste-mzdy
-        int HrubaMzda=0;
+        int HrubaMzda = 0;
         try {
             HrubaMzda = Integer.parseInt(editTextHrubaMzda.getText().toString());
         } catch (NumberFormatException e) {
-            showError("Chybný vstup!");
+            showError(getResources().getString(R.string.neplatny_vstup));
         }
         Double zdravotni = HrubaMzda * 0.09;
         Double socialni = HrubaMzda * 0.25;
-        int superhruba = HrubaMzda + zdravotni.intValue()+socialni.intValue();
+        int superhruba = HrubaMzda + zdravotni.intValue() + socialni.intValue();
         // zaokrouhlit superhrubou mzdu na stovky nahoru
         // superhruba je celé číslo, výsledek je zaokrouhlen sám
-        superhruba = ((superhruba+100)/100)*100;
+        superhruba = ((superhruba + 100) / 100) * 100;
         // zaloha zaokrouhlena na koruny
-        int zalohanadanzprijmu = (int) Math.round(superhruba*0.15);
+        int zalohanadanzprijmu = (int) Math.round(superhruba * 0.15);
         zalohanadanzprijmu -= 2070; // sleva na poplatníka
         Double zamsocialni = HrubaMzda * 0.065;
         Double zamzdravotni = HrubaMzda * 0.045;
         int CistaMzda = HrubaMzda - zalohanadanzprijmu
-                -zamsocialni.intValue() - zamzdravotni.intValue();
+                - zamsocialni.intValue() - zamzdravotni.intValue();
         textViewCistaMzda.setText(Integer.toString(CistaMzda));
         // při 11000 má vyjít 9640
     }
+
+    // show toast message
     private void showError(String zprava) {
         Toast myToast = Toast.makeText(
                 getApplication(), zprava,
