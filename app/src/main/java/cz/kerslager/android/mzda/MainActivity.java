732b580e53b0b7e09317e6ff70507a5a8719b410
Milan Kerslager <milan.kerslager@pslib.cz>
2017-03-18 00:13:38 +0100

    comments, variable type and names

--- app/src/main/java/cz/kerslager/android/mzda/MainActivity.java
+++ app/src/main/java/cz/kerslager/android/mzda/MainActivity.java
@@ -20,33 +20,34 @@ public class MainActivity extends AppCompatActivity {
         textViewCistaMzda = (TextView) findViewById(R.id.textViewCistaMzda);
     }
 
+    // výpočet čisté mzdy ze zadané hrubé mzdy (po stisknutí tlačítka)
     public void onButtonKlik(View view) {
         // https://www.vypocet.cz/popis-vypoctu-ciste-mzdy
-        int HrubaMzda = 0;
+        long hrubamzda = 0;
         try {
-            HrubaMzda = Integer.parseInt(editTextHrubaMzda.getText().toString());
+            hrubamzda = Integer.parseInt(editTextHrubaMzda.getText().toString());
         } catch (NumberFormatException e) {
-            showError(getResources().getString(R.string.neplatny_vstup));
+            showToast(getResources().getString(R.string.neplatny_vstup));
         }
-        Double zdravotni = HrubaMzda * 0.09;
-        Double socialni = HrubaMzda * 0.25;
-        int superhruba = HrubaMzda + zdravotni.intValue() + socialni.intValue();
+        Double zdravotni = hrubamzda * 0.09;
+        Double socialni = hrubamzda * 0.25;
+        long superhruba = hrubamzda + zdravotni.intValue() + socialni.intValue();
         // zaokrouhlit superhrubou mzdu na stovky nahoru
-        // superhruba je celé číslo, výsledek je zaokrouhlen sám
+        // superhruba je celé číslo, takže výpočet probíha celočíselně 
         superhruba = ((superhruba + 100) / 100) * 100;
-        // zaloha zaokrouhlena na koruny
-        int zalohanadanzprijmu = (int) Math.round(superhruba * 0.15);
+        // záloha na dan musí být zaokrouhlená na koruny
+        long zalohanadanzprijmu = Math.round(superhruba * 0.15);
         zalohanadanzprijmu -= 2070; // sleva na poplatníka
-        Double zamsocialni = HrubaMzda * 0.065;
-        Double zamzdravotni = HrubaMzda * 0.045;
-        int CistaMzda = HrubaMzda - zalohanadanzprijmu
+        Double zamsocialni = hrubamzda * 0.065;
+        Double zamzdravotni = hrubamzda * 0.045;
+        long CistaMzda = hrubamzda - zalohanadanzprijmu
                 - zamsocialni.intValue() - zamzdravotni.intValue();
-        textViewCistaMzda.setText(Integer.toString(CistaMzda));
-        // při 11000 má vyjít 9640
+        textViewCistaMzda.setText(Long.toString(CistaMzda));
+        // při 11000 hrubého musí vyjít 9640 (pouze při slevě na poplatníka)
     }
 
-    // show toast message
-    private void showError(String zprava) {
+    // zobrazit toast zprávu
+    private void showToast(String zprava) {
         Toast myToast = Toast.makeText(
                 getApplication(), zprava,
                 Toast.LENGTH_LONG
