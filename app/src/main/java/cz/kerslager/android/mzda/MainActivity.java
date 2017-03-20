a5e4a57b08224bf80cd167ab39ddc08c756a4f2e
Milan Kerslager <milan.kerslager@pslib.cz>
2017-03-21 00:13:19 +0100

    +poznamka (test)

--- app/src/main/java/cz/kerslager/android/mzda/MainActivity.java
+++ app/src/main/java/cz/kerslager/android/mzda/MainActivity.java
@@ -102,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+        // poznamka
     }
 
     // vytvoření textu pro okno "O programu"
